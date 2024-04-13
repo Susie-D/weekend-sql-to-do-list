5f80ec5142c27abc871ca81bd1d9265659791027
Susie Douang <soutsada.d@gmail.com>
2024-04-13 02:22:44 -0500

    udpate rendering for items that are complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,104 +1,118 @@
 // GET
 getTodos = () => {
-  axios({
-    method: 'GET',
-    url: '/todos',
-  })
-    .then((response) => {
-      renderToDo(response.data);
+    axios({
+        method: 'GET',
+        url: '/todos',
     })
-    .catch((error) => {
-      console.log('Axios GET error', error);
-      alert('Error retrieving todo list. Please try again later.');
-    });
+        .then((response) => {
+            renderToDo(response.data);
+        })
+        .catch((error) => {
+            console.log('Axios GET error', error);
+            alert('Error retrieving todo list. Please try again later.');
+        });
 };
 
 // RENDER GET
 renderToDo = (todoList) => {
-  document.getElementById('todoList').innerHTML = '';
-  for (todo of todoList) {
-    document.getElementById('todoList').innerHTML += `
+    document.getElementById('todoList').innerHTML = '';
+    for (todo of todoList) {
+        document.getElementById('todoList').innerHTML += `
     <tr id="todoRow" data-testid="toDoItem">
         <td class="row-line">
-        ${
-          todo.is_completed
-            ? ` <button onclick="editTodo(${todo.id}, ${todo.is_completed})" class="emo-button">✅</button>`
-            : `<button onclick="editTodo(${todo.id}, ${todo.is_completed})" class="emo-button">🔴</button>`
-        }
+        ${todo.is_completed
+                ? ` <button class="emo-button disable">✅</button>`
+                : `<button onclick="editTodo(${todo.id}, ${todo.is_completed})" class="emo-button">🔴</button>`
+            }
     </td>
-        <td>${todo.description}</td>
-        <td>${new Date(todo.due_date).toLocaleDateString()}</td>
-        <td> <button onclick="editTodo(${todo.id}, ${
-      todo.is_completed
-    })" class="emo-button">✏️</button></td>
         <td>
-            <button onclick="deleteTodo(${
-              todo.id
+               ${todo.is_completed
+                ? `<s>${todo.description}</s>`
+                : `${todo.description}`
+            }
+        
+        </td>
+        <td>
+        
+
+        
+        ${todo.is_completed
+                ? `<s>${new Date(todo.due_date).toLocaleDateString()}</s>`
+                : `        ${new Date(todo.due_date).toLocaleDateString()}`
+            }
+        
+        
+        </td>
+        <td> <button onclick="editTodo(${todo.id}, ${todo.is_completed
+            })" class="emo-button">✏️</button>
+    </td>
+        <td>
+            <button onclick="deleteTodo(${todo.id
             })" class="emo-button" data-testid="deleteButton">❌
             </button>
         </td>
     </tr>
     `;
-  }
+    }
 };
 
 // ADD
 addTodo = () => {
-  // Values to send to server
-  const description = document.getElementById('description').value;
-  let dueDate = document.getElementById('dueDate').value;
-  let completed = false;
-  let todoObj = { description, dueDate, completed };
-  axios({
-    method: 'POST',
-    url: '/todos',
-    data: todoObj,
-  })
-    .then((response) => {
-      getTodos();
-      clearInputs();
+    // Values to send to server
+    const description = document.getElementById('description').value;
+    let dueDate = document.getElementById('dueDate').value;
+    let completed = false;
+    let todoObj = { description, dueDate, completed };
+    axios({
+        method: 'POST',
+        url: '/todos',
+        data: todoObj,
     })
-    .catch((error) => {
-      console.log('Axios ADD error', error);
-      alert('Error adding an item to todo list. Please try again later.');
-    });
+        .then((response) => {
+            getTodos();
+            clearInputs();
+        })
+        .catch((error) => {
+            console.log('Axios ADD error', error);
+            alert('Error adding an item to todo list. Please try again later.');
+        });
 };
 
 clearInputs = () => {
-  document.getElementById('description').value = '';
-  document.getElementById('dueDate').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('dueDate').value = '';
 };
 
 deleteTodo = (todoId) => {
-  console.log('todoId', todoId);
-  axios({
-    method: 'DELETE',
-    url: `/todos/${todoId}`,
-  })
-    .then((response) => {
-      getTodos();
+    console.log('todoId', todoId);
+    axios({
+        method: 'DELETE',
+        url: `/todos/${todoId}`,
     })
-    .catch((error) => {
-      console.log('Axios DELETE error', error);
-      alert('Error deleting an item to todo list. Please try again later.');
-    });
+        .then((response) => {
+            getTodos();
+        })
+        .catch((error) => {
+            console.log('Axios DELETE error', error);
+            alert('Error deleting an item to todo list. Please try again later.');
+        });
 };
 
 // EDIT
 editTodo = (todoId, isCompleted) => {
-  axios({
-    method: 'PUT',
-    url: `/todos/is_completed/${todoId}`,
-    data: {
-      is_completed: isCompleted,
-    },
-  })
-    .then(function (response) {
-      getTodos();
+    axios({
+        method: 'PUT',
+        url: `/todos/is_completed/${todoId}`,
+        data: {
+            is_completed: isCompleted,
+        },
     })
-    .catch(function (error) {
-      alert('Updating item has an error. Please try again later.');
-    });
+        .then(function (response) {
+            getTodos();
+        })
+        .catch(function (error) {
+            alert('Updating item has an error. Please try again later.');
+        });
 };
 
 getTodos();
